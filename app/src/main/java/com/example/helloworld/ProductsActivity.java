001de61a322af48f5fc3790fb3d99b2a0d6d9d49
Lucia <cisneroslu@gmail.com>
2020-05-07 00:31:41 -0400

    productsActivity

--- app/src/main/java/com/example/helloworld/ProductsActivity.java
+++ app/src/main/java/com/example/helloworld/ProductsActivity.java
@@ -1,12 +1,20 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProductsActivity extends AppCompatActivity {
+
+    EditText precio;
+    EditText marca;
+    EditText cantidad;
+    ImageButton back_arrow;
 
 
     @Override
@@ -14,6 +22,20 @@ public class ProductsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_productos);
+
+        precio = findViewById(R.id.precio);
+        marca = findViewById(R.id.marca);
+        back_arrow = findViewById(R.id.backArrow2);
+
+        back_arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Intent intent;
+                intent = new Intent(ProductsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
