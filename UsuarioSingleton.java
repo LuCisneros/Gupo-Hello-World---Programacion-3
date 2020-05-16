44d1f349d068f2c2b2e488d0dc9d532ff3fb17ed
JanisMontecinos <62890044+JanisMontecinos@users.noreply.github.com>
2020-05-15 21:28:17 -0400

    Add files via upload

---
+++ UsuarioSingleton.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+public class UsuarioSingleton {
+    private static UsuarioSingleton instance;
+
+    public Usuario usuario;
+
+    private UsuarioSingleton() {
+    }
+
+    public static UsuarioSingleton getInstance() {
+        if (instance == null) {
+            instance = new UsuarioSingleton();
+        }
+        return instance;
+    }
+}
+
+
