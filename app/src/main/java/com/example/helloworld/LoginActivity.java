9d0037a8a43cdfdfc16a72721da3e8ccc4bfbb9d
Janis Montecinos <janimontdez@gmail.com>
2020-05-16 01:36:32 -0400

    One Time Login_2.1

--- app/src/main/java/com/example/helloworld/LoginActivity.java
+++ app/src/main/java/com/example/helloworld/LoginActivity.java
@@ -2,9 +2,11 @@ package com.example.helloworld;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Switch;
@@ -41,6 +43,14 @@ public class LoginActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar);
         cbRememberMe = findViewById(R.id.cbRememberMe);
 
+        userSharedPreferencesManager = new UserSharedPreferencesManager(this);
+
+        if (userSharedPreferencesManager.getUser().getUsuario() != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.putExtra("user", userSharedPreferencesManager.getUser());
+            startActivity(intent);
+        }
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,6 +74,18 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        adminSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if (checked) {
+                    Log.d("TEST", "SWITCH -> TRUE");
+                } else {
+                    Log.d("TEST", "SWITCH -> FALSE");
+                }
+            }
+        });
+
     }
 
     @Override
